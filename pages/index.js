ec6b743026934a89074d99db93f909b2b2418093
liran <857271370@qq.com>
2021-05-17 17:47:27 +0800

    Updated demo styles

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import AntdTestComponents from 'components/AntdTest';
+import Link from 'next/link';
 import './index.less';
 
 export default function Home() {
   return (
     <div className="container">
       <main className="main">
-        <div style={{ width: 800 }}>
+        <Link href="/tailwind">
+          <div className="text-5xl text-green-400 cursor-pointer hover:text-blue-400">Go to Tailwind</div>
+        </Link>
+        <div className="w-10/12">
           <AntdTestComponents />
         </div>
       </main>
